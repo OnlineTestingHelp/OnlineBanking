1e4d5991f117a7a478acdf6d7610ffb6bbab8910
Venkata Krishna <onlinetestinghelp@email.com>
2022-07-19 08:52:11 +0530

    Updated baseTest and properties file

--- src/test/java/com/ob/BaseTest/baseTest.java
+++ src/test/java/com/ob/BaseTest/baseTest.java
@@ -1,12 +1,19 @@
 package com.ob.BaseTest;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -14,6 +21,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import com.ob.utilities.Utilities;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
@@ -25,11 +33,12 @@ public class baseTest {
 	String browser="";
 	public static ExtentReports report;
 	public ExtentTest test;
+	String HubUrl;
 
 	@BeforeMethod
 	@Parameters("Browser")
-	public void launchingApp(String browser,Method method,ITestContext context) {
-		
+	public void launchingApp(String browser,Method method,ITestContext context) throws IOException {
+		HubUrl = Utilities.getProperty("HubUrl");
 		this.browser = browser;
 		test = report.startTest(method.getName());
 		driver = launchApp();
@@ -41,16 +50,22 @@ public class baseTest {
 		report.endTest(test);
 	}
 	
-	public WebDriver launchApp() {
+	public WebDriver launchApp() throws MalformedURLException {
 		if(browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.setCapability("platformName", "Windows");
+			driver = new RemoteWebDriver(new URL(HubUrl), chromeOptions);
 		}else if(browser.equalsIgnoreCase("Firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setCapability("platformName", "Windows");
+			driver = new RemoteWebDriver(new URL(HubUrl), firefoxOptions);
 		}else if(browser.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
-			driver = new InternetExplorerDriver();
+			EdgeOptions edgeOpt = new EdgeOptions();
+			edgeOpt.setCapability("platformName","Windows");
+			driver = new RemoteWebDriver(new URL(HubUrl), edgeOpt);
 		}
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		return driver;
